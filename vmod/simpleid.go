e9e2b127c64c149c53bbea935ff25c85398079ec
Dennis Kleber <dennis_kleber@mailbox.org>
2021-07-20 16:11:42 +0200

    add MID

--- vmod/simpleid.go
+++ vmod/simpleid.go
@@ -5,3 +5,15 @@ type SimpleID struct {
 	Name  string `bson:"name" json:"name"`
 	Model string `bson:"model" json:"model"`
 }
+
+type MID struct {
+	ID   string `bson:"id" json:"id"`
+	Type string `bson:"type" json:"type"`
+}
+
+func NewMID(id string, t string) *MID {
+	return &MID{
+		ID:   id,
+		Type: t,
+	}
+}
