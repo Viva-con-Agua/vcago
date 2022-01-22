3ab96bf23e3bb85e599278310e38ec15cab428b0
dennis <dennis_kleber@mailbox.org>
2022-01-22 15:19:21 +0100

    version 1.1.0

--- v2/vlog/error_handler.go
+++
@@ -1,9 +0,0 @@
-package vlog
-
-type ErrorResponse struct {
-	Status  int         `json:"-"`
-	Message string      `json:"message"`
-	Body    interface{} `json:"body,omitempty"`
-	Coll    string      `json:"collection,omitempty"`
-	Model   string      `json:"model,omitempty"`
-}
--- v2/vmod/defaults.go
+++
@@ -1,9 +0,0 @@
-package vmod
-
-type (
-    //CodeRequest used for handling LinkToken code.
-    CodeRequest struct {
-        Code string `json:"code"`
-        Case string `json:"case,omitempty"`
-    }
-)
