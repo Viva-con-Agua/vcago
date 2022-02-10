048fe1e3e6c8a34b855ba1c03cad08504ecd733e
dennis <dennis_kleber@mailbox.org>
2022-02-10 16:26:19 +0100

    add callback function for hydra clients

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Viva-con-Agua/vcago
 go 1.15
 
 require (
+	github.com/coreos/go-oidc/v3 v3.1.0
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-stack/stack v1.8.1 // indirect
@@ -20,6 +21,7 @@ require (
 	go.mongodb.org/mongo-driver v1.8.2
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/net v0.0.0-20220121210141-e204ce36a2ba // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
---
+++ hydra.go
@@ -0,0 +1,75 @@
+package vcago
+
+import (
+	"context"
+	"log"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"golang.org/x/oauth2"
+)
+
+type HydraClient struct {
+	Oauth2Config oauth2.Config
+	Verifier     *oidc.IDTokenVerifier
+}
+
+func NewHydraClient() (r *HydraClient) {
+	ctx := context.Background()
+	r = new(HydraClient)
+	provider, err := oidc.NewProvider(ctx, Config.GetEnvString("OIDC_HOST", "w", "http://hydra.localhost/"))
+	if err != nil {
+		log.Print(err)
+	}
+	// Configure an OpenID Connect aware OAuth2 client.
+
+	r.Oauth2Config = oauth2.Config{
+		ClientID:     Config.GetEnvString("OIDC_CLIENT_ID", "w", "test"),
+		ClientSecret: Config.GetEnvString("OIDC_CLIENT_SECRET", "w", "secret"),
+		RedirectURL:  Config.GetEnvString("OIDC_REDIRECT_URL", "w", "http://localhost:8081/callback"),
+
+		// Discovery returns the OAuth2 endpoints.
+		Endpoint: provider.Endpoint(),
+
+		// "openid" is a required scope for OpenID Connect flows.
+		Scopes: []string{oidc.ScopeOpenID, "extra_vars"},
+	}
+	r.Verifier = provider.Verifier(&oidc.Config{ClientID: r.Oauth2Config.ClientID})
+	return
+}
+
+type Callback struct {
+	Code string `json:"code"`
+}
+
+type UserClaims struct {
+	User User `json:"user"`
+}
+
+func (i *HydraClient) Callback(ctx context.Context, callback *Callback) (r *User, err error) {
+	oauth2Token := new(oauth2.Token)
+	oauth2Token, err = i.Oauth2Config.Exchange(ctx, callback.Code)
+	if err != nil {
+		return nil, NewStatusInternal(err)
+	}
+	// Extract the ID Token from OAuth2 token.
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		// handle missing token
+	}
+	// Parse and verify ID Token payload.
+	idToken := new(oidc.IDToken)
+	idToken, err = i.Verifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		return nil, NewStatusInternal(err)
+	}
+	resp := struct {
+		OAuth2Token   *oauth2.Token
+		IDTokenClaims *UserClaims // ID Token payload is just JSON.
+	}{oauth2Token, new(UserClaims)}
+
+	if err = idToken.Claims(&resp.IDTokenClaims); err != nil {
+		return
+	}
+	r = &resp.IDTokenClaims.User
+	return
+}
