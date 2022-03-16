c09677b8e557a4bd1c9dab85c670162efde3466a
dennis <dennis_kleber@mailbox.org>
2022-03-16 11:55:08 +0100

    nats

--- nats.go
+++ nats.go
@@ -10,19 +10,20 @@ import (
 type NatsDAO struct {
 	host       string
 	port       string
+	skip       bool
 	connection *nats.EncodedConn
 }
 
+//Nats used for Nats connection
 var Nats = new(NatsDAO)
 
-func (i *NatsDAO) LoadEnv() *NatsDAO {
-	var l LoadEnv
-	i.host = l.GetEnvString("NATS_HOST", "w", "localhost")
-	i.port = l.GetEnvString("NATS_PORT", "w", "4222")
-	return i
-}
-
-func (i *NatsDAO) Connect() (r *NatsDAO) {
+func (i *NatsDAO) Connect() {
+	i.skip = Config.GetEnvBool("NATS_SKIP", "n", false)
+	if i.skip {
+		return
+	}
+	i.host = Config.GetEnvString("NATS_HOST", "w", "localhost")
+	i.port = Config.GetEnvString("NATS_PORT", "w", "4222")
 	natsUrl := "nats://" + i.host + ":" + i.port
 	nc, err := nats.Connect(natsUrl)
 	if err != nil {
@@ -33,15 +34,20 @@ func (i *NatsDAO) Connect() (r *NatsDAO) {
 		log.Fatal(err)
 	}
 	log.Print("nats successfully connected!")
-	return i
+	return
 }
 
 func (i *NatsDAO) Publish(message string, body interface{}) {
+	if i.skip {
+		return
+	}
 	i.connection.Publish(message, body)
 }
 
 func (i *NatsDAO) Subscribe(message string, catch interface{}) {
-
+	if i.skip {
+		return
+	}
 	_, err := i.connection.Subscribe(message, catch)
 	if err != nil {
 		//TODO: nats logging message
