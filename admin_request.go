60e4fc1de97e82d9523fca7681ee413c90d5622f
dennis <dennis_kleber@mailbox.org>
2022-05-02 22:46:17 +0200

    admin request post

--- admin_request.go
+++ admin_request.go
@@ -1,7 +1,9 @@
 package vcago
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -49,4 +51,47 @@ func (i *AdminRequest) Get(path string) (r *Response, err error) {
 		return
 	}
 	return
+}
+
+func (i *AdminRequest) Post(path string, data interface{}) (r *Response, err error) {
+	var jsonData []byte
+	if jsonData, err = json.Marshal(data); err != nil {
+		return
+	}
+	url := i.URL + path
+	request := new(http.Request)
+	if request, err = http.NewRequest("POST", url, bytes.NewBuffer(jsonData)); err != nil {
+		return
+	}
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	client := &http.Client{}
+	response := new(http.Response)
+	response, err = client.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	var bodyBytes []byte
+	if response.StatusCode != 201 {
+		if response.StatusCode == 404 {
+			return nil, errors.New("request failed with status 404")
+		}
+		if bodyBytes, err = ioutil.ReadAll(response.Body); err != nil {
+			return
+		}
+		body := new(interface{})
+		if err = json.Unmarshal(bodyBytes, body); err != nil {
+			return
+		}
+		return nil, errors.New(response.Status)
+	}
+	r = new(Response)
+	if bodyBytes, err = ioutil.ReadAll(response.Body); err != nil {
+		return
+	}
+	if err = json.Unmarshal(bodyBytes, r); err != nil {
+		return
+	}
+	return
+
 }
