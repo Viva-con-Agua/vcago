09d13e843fee9666c8a54ed4521e756c17f77495
Dennis Kleber <dennis_kleber@mailbox.org>
2022-09-30 17:35:56 +0200

    add bad request to context

--- context.go
+++ context.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"net/http"
 	"strings"
 
 	"github.com/golang-jwt/jwt"
@@ -11,18 +12,20 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-//Context extends an echo.Context type. Inititial an Context for an echo.Group via
-//echo.Group.Use(Handler.Context()). The Context model is set by the Model param of the Handler model.
+// Context extends an echo.Context type. Inititial an Context for an echo.Group via
+// echo.Group.Use(Handler.Context()). The Context model is set by the Model param of the Handler model.
 //
-//Example:
+// Example:
+//
 //	func (i *ExampleHandler) Routes(group echo.Group) {
 //		group.Use(i.Context)
 //	}
 //
-//Echo need and function that provide an echo.Context as parameter.
-//So you need to convert the echo.Context to an Context in a function block.
+// Echo need and function that provide an echo.Context as parameter.
+// So you need to convert the echo.Context to an Context in a function block.
 //
-//Example:
+// Example:
+//
 //	func (i *ExampleHandler) Create(cc echo.Context) (err error) {
 //		c := cc.(vcago.Context)
 //		...
@@ -32,15 +35,15 @@ type Context struct {
 	echo.Context
 }
 
-//Ctx return echo.Context.Context().
+// Ctx return echo.Context.Context().
 func (i *Context) Ctx() context.Context {
 	return i.Request().Context()
 }
 
-//BindAndValidate binds the request data in an the body interface.
-//Define param:"" for bind the params in a struct.
-//Define json:"" for bind the request body as json in a struct.
-//Define query:"" for bind the query parameters in a struct.
+// BindAndValidate binds the request data in an the body interface.
+// Define param:"" for bind the params in a struct.
+// Define json:"" for bind the request body as json in a struct.
+// Define query:"" for bind the query parameters in a struct.
 func (i *Context) BindAndValidate(body interface{}) error {
 	if err := i.Bind(body); err != nil {
 		return NewError(err, "DEBUG", "bind").AddModel(i.Model)
@@ -51,8 +54,8 @@ func (i *Context) BindAndValidate(body interface{}) error {
 	return nil
 }
 
-//AccessToken binds the accessToken form an cookie into the token interface.
-//The token needs to extends jwt.StandardClaims.
+// AccessToken binds the accessToken form an cookie into the token interface.
+// The token needs to extends jwt.StandardClaims.
 func (i *Context) AccessToken(token interface{}) (err error) {
 	t := i.Get("token")
 	if t == nil {
@@ -67,7 +70,7 @@ func (i *Context) AccessToken(token interface{}) (err error) {
 	return
 }
 
-//RefreshToken returns the user id of an refresh token.
+// RefreshToken returns the user id of an refresh token.
 func (i *Context) RefreshTokenID() (string, error) {
 	token := i.Get("token").(*jwt.Token)
 	if token == nil {
@@ -76,13 +79,14 @@ func (i *Context) RefreshTokenID() (string, error) {
 	return token.Claims.(*RefreshToken).UserID, nil
 }
 
-//ErrorResonse match the error and returns the correct error response.
+// ErrorResonse match the error and returns the correct error response.
 //
-//Error: mongo.IsDuplicateKeyError
+// Error: mongo.IsDuplicateKeyError
 //
-//Status: 409 Conflict
+// Status: 409 Conflict
 //
-//JSON:
+// JSON:
+//
 //	{
 //		"type": "error",
 //		"message": "duplicate_key_error",
@@ -90,22 +94,24 @@ func (i *Context) RefreshTokenID() (string, error) {
 //		"payload": key from error
 //	}
 //
-//Error: mongo.ErrNoDocument
+// Error: mongo.ErrNoDocument
 //
-//Status: 404 Not Found
+// Status: 404 Not Found
 //
-//JSON:
+// JSON:
+//
 //	{
 //		"type": "error",
 //		"message": "not_found",
 //		"model": model
 //	}
 //
-//Error: default
+// Error: default
 //
-//Status: 500 Internal Server Error
+// Status: 500 Internal Server Error
 //
-//JSON:
+// JSON:
+//
 //	{
 //		"type": "error",
 //		"message": "internal_server_error",
@@ -118,8 +124,8 @@ func (i *Context) ErrorResponse(err error) error {
 	return i.JSON(NewInternalServerError(i.Model).Response())
 }
 
-//Log return a function call for handling Debug and Error logs.
-//Usage: c.Log(err)(err)
+// Log return a function call for handling Debug and Error logs.
+// Usage: c.Log(err)(err)
 func (i *Context) Log(err error) {
 	id := i.Request().Header.Get(echo.HeaderXRequestID)
 	if id == "" {
@@ -134,11 +140,12 @@ func (i *Context) Log(err error) {
 	output.Print(id)
 }
 
-//Created returns an Created response.
+// Created returns an Created response.
 //
-//Status: 201 Created
+// Status: 201 Created
 //
-//JSON:  model == "example"
+// JSON:  model == "example"
+//
 //	{
 //		"type": "success",
 //		"message": "successfully_created",
@@ -149,11 +156,12 @@ func (i *Context) Created(payload interface{}) (err error) {
 	return i.JSON(NewCreated(i.Model, payload).Response())
 }
 
-//Selected returns an Selected response.
+// Selected returns an Selected response.
 //
-//Status: 200 OK
+// Status: 200 OK
 //
-//JSON:  model == "example"
+// JSON:  model == "example"
+//
 //	{
 //		"type": "success",
 //		"message": "successfully_selected",
@@ -164,11 +172,12 @@ func (i *Context) Selected(payload interface{}) (err error) {
 	return i.JSON(NewSelected(i.Model, payload).Response())
 }
 
-//Listed return an List response.
+// Listed return an List response.
 //
-//Status: 200 OK
+// Status: 200 OK
 //
-//JSON:  model == "example"
+// JSON:  model == "example"
+//
 //	{
 //		"type": "success",
 //		"message": "successfully_selected",
@@ -179,11 +188,12 @@ func (i *Context) Listed(payload interface{}) (err error) {
 	return i.JSON(NewSelected(i.Model+"_list", payload).Response())
 }
 
-//Updated returns an Updated response
+// Updated returns an Updated response
 //
-//Status: 200 OK
+// Status: 200 OK
 //
-//JSON:  model == "example"
+// JSON:  model == "example"
+//
 //	{
 //		"type": "success",
 //		"message": "successfully_updated",
@@ -194,11 +204,12 @@ func (i *Context) Updated(payload interface{}) (err error) {
 	return i.JSON(NewUpdated(i.Model, payload).Response())
 }
 
-//Deleted returns an Deleted response.
+// Deleted returns an Deleted response.
 //
-//Status: 200 OK
+// Status: 200 OK
 //
-//JSON:  model == "example"
+// JSON:  model == "example"
+//
 //	{
 //		"type": "success",
 //		"message": "successfully_deleted",
@@ -209,11 +220,12 @@ func (i *Context) Deleted(payload interface{}) (err error) {
 	return i.JSON(NewDeleted(i.Model, payload).Response())
 }
 
-//SuccessResponse returns an new success 200 OK response with an custom message string.
+// SuccessResponse returns an new success 200 OK response with an custom message string.
 //
-//Status: 200 OK
+// Status: 200 OK
 //
-//JSON:  model == "example"
+// JSON:  model == "example"
+//
 //	{
 //		"type": "success",
 //		"message": message,
@@ -222,6 +234,10 @@ func (i *Context) Deleted(payload interface{}) (err error) {
 //	}
 func (i *Context) SuccessResponse(status int, message string, model string, payload interface{}) (err error) {
 	return i.JSON(NewResp(status, "success", message, model, payload).Response())
+}
+
+func (i *Context) BadRequest(message string, payload interface{}) (err error) {
+	return i.JSON(NewResp(http.StatusBadRequest, "bad_request", message, i.Model, payload).Response())
 }
 
 func getCollectionFromDupKey(err error) string {
