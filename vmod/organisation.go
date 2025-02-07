de61c2acf977c29285d35518d5c526cf54215825
Tobias Kästle <t.kaestle@vivaconagua.org>
2025-02-07 14:03:20 +0100

    added organisation

--- vmod/organisation.go
+++ vmod/organisation.go
@@ -5,12 +5,13 @@ import "errors"
 type (
 	//User represents the user default user information they are shared with all viva con agua services.
 	Organisation struct {
-		ID           string `json:"id" bson:"_id"`
-		Email        string `json:"email" bson:"email"`
-		Name         string `json:"name" bson:"name"`
-		Country      string `json:"country" bson:"country"`
-		Abbreviation string `json:"abbreviation" bson:"abbreviation"`
-		DefaultAspID string `json:"default_asp_id" bson:"default_asp_id"`
+		ID           string   `json:"id" bson:"_id"`
+		Email        string   `json:"email" bson:"email"`
+		Name         string   `json:"name" bson:"name"`
+		Country      string   `json:"country" bson:"country"`
+		Abbreviation string   `json:"abbreviation" bson:"abbreviation"`
+		DefaultAspID string   `json:"default_asp_id" bson:"default_asp_id"`
+		Modified     Modified `json:"modified" bson:"modified"`
 	}
 )
 
